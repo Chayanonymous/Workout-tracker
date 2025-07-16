1a611bf5bac29f4a68a1bdb6da13ffc6199bb442
Chayandeep <chayansingh05@gmail.com>
2025-07-15 22:31:17 -0400

    made a class to record the workouts

--- WorkoutTracker/src/ExersiceRecorder.java
+++ WorkoutTracker/src/ExersiceRecorder.java
@@ -1,6 +1,26 @@
 import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ExersiceRecorder {
 	
+	public static void writeToFile(List<Exersice> workouts) {
+		FileWriter fWriter = null;
+		try {
+			fWriter = new FileWriter("workout_recorder.txt",true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try(BufferedWriter writer = new BufferedWriter(fWriter)){
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
